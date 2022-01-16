750e2b8337f7ce9b82f0ce5bb7b00aa41a99628c
Chin Man KWAN <kwanchmn@gmail.com>
2022-01-16 12:31:58 +0100

    Just in case a backup for the final results.

--- thesis_nlp/docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use thesis_nlp in a project::
-
-    import thesis_nlp
--- thesis_nlp/thesis_nlp/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Top-level package for thesis_nlp."""
-
-__author__ = """Chin Man KWAN"""
-__email__ = 'chinman.kwan@graduateinstitute.ch'
-__version__ = '0.1.0'
-
-import thesis_nlp
